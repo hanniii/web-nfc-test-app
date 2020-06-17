477ab1325a7ca20858169dc08533c64c8116b038
Hanna <hanna-zalami@gmx.de>
2020-06-17 16:35:31 +0200

    keine ahung wie vielter commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React from 'react';
-//import logo from './logo.svg';
 import './App.css';
-//import 'nfc.js';
+import Hello from "./sayHello";
 
 function App() {
   return (
-    <div className="App">
-        <div class="wrapper">
-
-          <h1 class="p-3">Scann your NFC Tag: </h1>
-
-          <div class="nfc-image w-50" id="nfccard">
-            <p>nfc logo</p>
-          </div>
-
-          <p>
-            <button onclick="readTag()">Test NFC Read</button>
-            <button onclick="writeTag()">Test NFC Write</button>
-          </p>
-          <pre id="log"></pre>
-          <p>Copyright &copy; All rights reserved | Hanna Zalami 2020</p>
-
-        </div>
+    <div>
+      <h1>Hello React</h1>
     </div>
   );
 }
