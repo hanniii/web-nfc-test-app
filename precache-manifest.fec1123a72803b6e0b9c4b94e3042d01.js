1e3b0ed9e59623176164991aeba7ba2e324e247b
Hanna <hanna-zalami@gmx.de>
2020-06-17 12:34:26 +0200

    Updates

---
+++ precache-manifest.fec1123a72803b6e0b9c4b94e3042d01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d4849e3802e214f1ab4a798a10cc125",
+    "url": "/web-nfc-test-app/index.html"
+  },
+  {
+    "revision": "30b06f7ba48f43b8e85c",
+    "url": "/web-nfc-test-app/static/css/main.ca455098.chunk.css"
+  },
+  {
+    "revision": "6da6e7f5c4cb3f804474",
+    "url": "/web-nfc-test-app/static/js/2.24b22568.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-nfc-test-app/static/js/2.24b22568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b06f7ba48f43b8e85c",
+    "url": "/web-nfc-test-app/static/js/main.28239b14.chunk.js"
+  },
+  {
+    "revision": "1cb8a525bf1728755523",
+    "url": "/web-nfc-test-app/static/js/runtime-main.e34dac42.js"
+  }
+]);
