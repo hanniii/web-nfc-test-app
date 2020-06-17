f93ff6e1e022c9e9a7b8cbf2465d69374327d891
Hanna <hanna-zalami@gmx.de>
2020-06-17 19:25:05 +0200

    19:24 Uhr am 17.06.2020

--- src/App.js
+++ src/App.js
@@ -1,42 +1,65 @@
 import React from 'react';
 import './App.css';
-//import Tweet from './tweet';
 
+/*global NDEFReader*/
+async function readTag(){
+  if ("NDEFReader" in window) {
+    const reader = new NDEFReader();
+    try {
+      await reader.scan();
+      reader.onreading = event => {
+        const decoder = new TextDecoder();
+        for (const record of event.message.records) {
+          consoleLog("Record type:  " + record.recordType);
+          //document.getElementById("log").innerHTML = "Record type:  " + record.recordType;
+          consoleLog("MIME type:    " + record.mediaType);
+          //document.getElementById("log").innerHTML = "MIME type:    " + record.mediaType;
+          consoleLog("=== data ===\n" + decoder.decode(record.data));
+          //document.getElementById("log").innerHTML = "=== data ===\n" + decoder.decode(record.data);
+        }
+      }
+    } catch(error) {
+      consoleLog(error);
+      //document.getElementById("log").innerHTML = "Error";
+    }
+  } else {
+    consoleLog("Web NFC is not supported.");
+    //document.getElementById("log").innerHTML = "Web NFC is not supported.";
+  }
+}
+
+/*global NDEFWriter*/
+async function writeTag() {
+  if ("NDEFWriter" in window) {
+    const writer = new NDEFWriter();
+    try {
+      await writer.write("What Web Can Do Today");
+      consoleLog("NDEF message written!");
+      //document.getElementById("log").innerHTML = "NDEF message written!";
+    } catch(error) {
+      consoleLog(error);
+      //document.getElementById("log").innerHTML = "Error";
+    }
+  } else {
+    consoleLog("Web NFC is not supported.");
+    //document.getElementById("log").innerHTML = "Web NFC is not supported.";
+  }
+}
+
+function consoleLog(data) {
+  var logElement = document.getElementById('log');
+  logElement.innerHTML += data + '\n' + '</br>';
+};
 
 function App() {
-  
-  const scanNFC = () => {
-    console.log("Scanne NFC");
-  }
-
-  const writeNFC = () => {
-    console.log("Schreibe NFC");
-  }
-
   return (
     <div className="app">
-
-      <h1 class="p-3">Scann your NFC Tag: </h1>
-
-      <div class="nfc-image w-50" id="nfccard">
-        <p>NFC Logo</p>
-      </div>
-
-      <p>
-        <button onClick={scanNFC}>Test NFC Read</button>
-        <button onClick={writeNFC}>Test NFC Write</button>
-      </p>
-      <pre id="log"></pre>
-
-      <p>Copyright &copy; All rights reserved | Hanna Zalami 2020</p>
+      <h1>Scann your NFC Tag: </h1>
+      <button onClick={readTag}>Scannen</button>
+      <button onClick={writeTag}>Schreiben</button>
+      <p id="log"></p>
     </div>
   );
 }
 
 export default App;
-
-
-
-//<h1>Hello React</h1>
-//<Tweet name="Dev ED"/>
-//<Tweet name="John Snow"/>*/
