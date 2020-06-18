d36c3b5ba991eb06f5fe50b9c41fc761f584a580
Hanna <hanna-zalami@gmx.de>
2020-06-18 15:53:08 +0200

    15:53 Uhr am 18.06.2020

--- src/App.js
+++ src/App.js
@@ -45,11 +45,14 @@ function consoleLog(data) {
 
 function App() {
   return (
-    <div className="app">
-      <h1>Scann your NFC Tag: </h1>
-      <button onClick={readTag}>Scannen</button>
-      <button onClick={writeTag}>Schreiben</button>
-      <p id="log"></p>
+    <div className="app text-center">
+      <div className="img cen">
+        <img src="" class="img-fluid rounded" alt=""></img>
+      </div>
+      <div id="log"></div>
+      <h3>Scann your NFC Tag: </h3>
+      <button type="button" className="btn" onClick={readTag}>Scannen</button>
+      <button type="button" className="btn" onClick={writeTag}>Schreiben</button>
     </div>
   );
 }
